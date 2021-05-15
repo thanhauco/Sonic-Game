08ceb67ff3856139a7f2074c015f1f05f368ff60
Thanh Vu <thanhauco@gmail.com>
2021-05-15 15:00:00 -0700

    fix(monitor): prevent multiple collections from same monitor box

--- src/entities/Monitor.js
+++ src/entities/Monitor.js
@@ -6,6 +6,7 @@ export default class Monitor extends Phaser.Physics.Arcade.Sprite {
     
     this.scene = scene;
     this.powerUpType = type;
+    this.isDestroyed = false;
     
     scene.add.existing(this);
     scene.physics.add.existing(this);
@@ -29,6 +30,8 @@ export default class Monitor extends Phaser.Physics.Arcade.Sprite {
   }
 
   hit(player) {
+    if (this.isDestroyed) return; 
+    this.isDestroyed = true;
     
     this.isDestroyed = true;
     
