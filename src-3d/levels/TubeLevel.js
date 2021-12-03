fc0b6eb3c1259c8a63c2d7c07b631580d92a60a2
Thanh Vu <thanhauco@gmail.com>
2021-12-03 09:00:00 -0800

    feat(3d): implement tube level with instanced rendering for rings

---
+++ src-3d/levels/TubeLevel.js
@@ -0,0 +1,138 @@
+import * as THREE from 'three';
+import { GameConfig3D } from '../config/GameConfig3D.js';
+
+export default class TubeLevel {
+    constructor(scene, assetLoader) {
+        this.scene = scene;
+        this.assetLoader = assetLoader;
+        
+        this.chunks = [];
+        this.chunkLength = GameConfig3D.CHUNK_LENGTH;
+        this.visibleChunks = GameConfig3D.VISIBLE_CHUNKS;
+        
+        this.rings = [];
+        this.obstacles = [];
+        
+        // Use InstancedMesh for high performance ring rendering
+        this.setupInstancedRings();
+        
+        // Initial chunks
+        for (let i = 0; i < this.visibleChunks; i++) {
+            this.createChunk(i * this.chunkLength);
+        }
+    }
+
+    setupInstancedRings() {
+        const torusGeom = new THREE.TorusGeometry(0.5, 0.1, 8, 16);
+        const ringMat = this.assetLoader.materials.get('ring');
+        this.ringInstances = new THREE.InstancedMesh(torusGeom, ringMat, 500);
+        this.ringInstances.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
+        this.ringInstances.castShadow = true;
+        this.scene.add(this.ringInstances);
+        
+        this.activeRings = [];
+    }
+
+    createChunk(zOffset) {
+        const group = new THREE.Group();
+        group.position.z = zOffset;
+        
+        // Track Floor
+        const floorGeom = new THREE.BoxGeometry(GameConfig3D.LANE_WIDTH * 3, 1, this.chunkLength);
+        const floor = new THREE.Mesh(floorGeom, this.assetLoader.materials.get('grass'));
+        floor.position.set(0, -0.5, this.chunkLength / 2);
+        floor.receiveShadow = true;
+        group.add(floor);
+        
+        // Decorative pillars/trees
+        for (let i = 0; i < 5; i++) {
+            const tree = this.assetLoader.createTreeModel();
+            tree.position.set(
+                (Math.random() > 0.5 ? 1 : -1) * (GameConfig3D.LANE_WIDTH * 2 + Math.random() * 5),
+                0,
+                Math.random() * this.chunkLength
+            );
+            group.add(tree);
+        }
+        
+        // Spawn rings in patterns
+        this.spawnRingPattern(zOffset);
+        
+        this.scene.add(group);
+        this.chunks.push({ z: zOffset, group });
+    }
+
+    spawnRingPattern(zOffset) {
+        const laneX = [-GameConfig3D.LANE_WIDTH, 0, GameConfig3D.LANE_WIDTH];
+        const startZ = zOffset + 20;
+        const count = 5;
+        const selectedLane = laneX[Math.floor(Math.random() * 3)];
+        
+        for (let i = 0; i < count; i++) {
+            this.activeRings.push({
+                x: selectedLane,
+                y: 1.5,
+                z: startZ + i * 4,
+                collected: false
+            });
+        }
+    }
+
+    update(playerZ) {
+        // Update Ring Instances
+        const matrix = new THREE.Matrix4();
+        let instanceIdx = 0;
+        
+        const time = Date.now() * 0.005;
+        
+        this.activeRings.forEach((ring, idx) => {
+            if (ring.collected) return;
+            
+            // Rotation and floating effect
+            matrix.makeRotationY(time);
+            matrix.setPosition(ring.x, ring.y + Math.sin(time + ring.z) * 0.2, ring.z);
+            this.ringInstances.setMatrixAt(instanceIdx++, matrix);
+        });
+        
+        this.ringInstances.count = instanceIdx;
+        this.ringInstances.instanceMatrix.needsUpdate = true;
+        
+        // Chunk management (Infinite scrolling)
+        if (playerZ > this.chunks[0].z + this.chunkLength) {
+            const oldChunk = this.chunks.shift();
+            this.scene.remove(oldChunk.group);
+            
+            const lastZ = this.chunks[this.chunks.length - 1].z;
+            this.createChunk(lastZ + this.chunkLength);
+            
+            // Cleanup old rings
+            this.activeRings = this.activeRings.filter(r => r.z > playerZ - 50);
+        }
+    }
+
+    checkCollisions(player) {
+        // Simple bounding box collision for rings
+        const pX = player.position.x;
+        const pY = player.position.y;
+        const pZ = player.position.z;
+        
+        this.activeRings.forEach(ring => {
+            if (ring.collected) return;
+            
+            const dx = Math.abs(pX - ring.x);
+            const dy = Math.abs(pY - ring.y);
+            const dz = Math.abs(pZ - ring.z);
+            
+            if (dx < 1 && dy < 1.5 && dz < 1) {
+                ring.collected = true;
+                this.onRingCollected();
+            }
+        });
+    }
+
+    onRingCollected() {
+        window.gameState.rings++;
+        window.gameState.score += GameConfig3D.RING_SCORE;
+        // Trigger event for HUD
+    }
+}
